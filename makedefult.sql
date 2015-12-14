f41866da1ae29d56c68429028f6cc6fc8a5a5c71
darthvader17 <fannara14@ru.is>
2015-12-14 13:06:53 +0000

    fdsafa

--- makedefult.sql
+++ makedefult.sql
@@ -7,11 +7,11 @@ name varchar(250),
 exchange varchar(250),
 category varchar(250),
 primary key(ticker)
-)
+);
 
 create table data(
-ticker varchar(250)
-dags date
+ticker varchar(250),
+dags date,
 open numeric(8,2),
 high numeric(8,2),
 low numeric(8,2),
@@ -20,7 +20,7 @@ volume numeric(8,2),
 adj close numeric(8,2),
 primary key(ticker,dags),
 foreign key(ticker) references company(ticker)
-)
+);
 
 create table exchange(
 exch varchar(250),
@@ -28,4 +28,4 @@ fjoldi int,
 land varchar(250),
 primary key(exch),
 foreign key(exch) references company(exch)
-)
+);
