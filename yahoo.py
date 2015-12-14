7c64df6529fffb36a7a8e72deac3674f73dafca5
darthvader17 <fannara14@ru.is>
2015-12-14 15:19:46 +0000

    fdsafas

--- yahoo.py
+++ yahoo.py
@@ -26,19 +26,19 @@ end = today
 
 
 ticker = pd.read_csv('YahooStockTickerSymbols.csv', sep=';',encoding='utf8')
-#Exchange=pd.read_csv('Exchange.csv',sep=';',encoding='utf-8')
-
-#for i in range(len(Exchange)):
-#	command="""insert into data (exch,fjoldi,land) values ('{}','{}','{}')""".format(Exchange.iloc[i,0],Exchange.iloc[i,1],Exchange.iloc[i,2],Exchange.iloc[i,3])
-#	cursor.execute(command)
+Exchange=pd.read_csv('YahooTickSymb.csv',sep=',',encoding='utf-8')
+for i in range(len(Exchange)):
+	command="""insert into exchange(exch,fjoldi,land,heimsalfa) values ('{}','{}','{}','{}')""".format(Exchange.iloc[i,0],Exchange.iloc[i,1],Exchange.iloc[i,2],Exchange.iloc[i,3])
+	cursor.execute(command)
 for i in range(len(ticker)):
-	command="""insert into data (ticker,name,exchange,category) values ('{}','{}','{}','{}')""".format(ticker.iloc[i,0],ticker.iloc[i,1],ticker.iloc[i,2],ticker.iloc[i,3])
+	command="""insert into company(ticker,name,exchange,category) values ('{}','{}','{}','{}')""".format(ticker.iloc[i,0],ticker.iloc[i,1],ticker.iloc[i,2],ticker.iloc[i,3])
 	print(command)
-#	cursor.execute(command)
-#	x = web.DataReader(ticker.iloc[i,0],'yahoo',start,end)
-#	for i in range(len(x)):
-#		command="""insert into data (ticker,date,open,high,low,close,volume, adjclose) values ('{}','{}','{}','{}','{}','{}','{}','{}')""".format(ticker,x.index[i],x.iloc[i,0],x.iloc[i,1],x.iloc[i,2],x.iloc[i,3],x.iloc[i,4],x.iloc[i,5])
-#		cursor.execute(command)
+	cursor.execute(command)
+	x = web.DataReader(ticker.iloc[i,0],'yahoo',start,end)
+	for i in range(len(x)):
+		command="""insert into data(ticker,date,open,high,low,close,volume,adjclose) values ('{}','{}','{}','{}','{}','{}','{}','{}')""".format(ticker,x.index[i],x.iloc[i,0],x.iloc[i,1],x.iloc[i,2],x.iloc[i,3],x.iloc[i,4],x.iloc[i,5])
+		print(command)
+		cursor.execute(command)
 
 conn.commit()
 cursor.close()
