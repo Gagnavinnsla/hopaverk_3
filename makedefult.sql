c3f13fb07a1f4c627f67fb6421a9f5b607e4dd50
darthvader17 <fannara14@ru.is>
2015-12-14 13:26:10 +0000

    fsdafa

--- makedefult.sql
+++ makedefult.sql
@@ -1,10 +1,17 @@
 create database stocks;
 \c stocks
 
+create table exchange(
+exch varchar(250),
+fjoldi int,
+land varchar(250),
+primary key(exch)
+);
+
 create table company(
 ticker varchar(250),
 name varchar(250),
-exchange varchar(250),
+exchange varchar(250) references exchange(exch),
 category varchar(250),
 primary key(ticker)
 );
@@ -12,20 +19,12 @@ primary key(ticker)
 create table data(
 ticker varchar(250),
 dags date,
-open numeric(8,2),
-high numeric(8,2),
-low numeric(8,2),
-close numeric(8,2),
-volume numeric(8,2),
-adj close numeric(8,2),
+open float(2),
+high float(2),
+low float(2),
+close float(2),
+volume float(2),
+adjclose float(2),
 primary key(ticker,dags),
 foreign key(ticker) references company(ticker)
-);
-
-create table exchange(
-exch varchar(250),
-fjoldi int,
-land varchar(250),
-primary key(exch),
-foreign key(exch) references company(exch)
 );
