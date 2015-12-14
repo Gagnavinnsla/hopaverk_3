085a23f37e3774f3ea068d2f671ea7e6a8e3f599
davidhal14 <davidhall14@ru.is>
2015-12-14 21:07:45 +0000

    ommi

--- Valmynd.py
+++ Valmynd.py
@@ -1,15 +1,35 @@
+import pandas as pd 
+import psycopg2
+import getpass
+
+host = 'localhost'
+dbname = 'stocks'
+
+username = 'postgres'
+pw = 'postgres'
+conn_string = "host='{}' dbname='{}' user='{}' password='{}'".format(host, dbname, username, pw)
+
+print("Connecting to database {}.{} as {}".format(host, dbname, username))
+
+conn = psycopg2.connect(conn_string)
+
+cursor = conn.cursor()
+
+
 #Valmynd
-def printnames(Text):
-	cursor.execute(Text)
-	List=cursor.fetchall()
-	if F!=[]:
-		Bool=True
-	else:
-		print('Nafnið er vitlaust eða gögn ekki til')
-	for i n range(len(F)):
-		print(i,F[i][0])
-	Tmp=int(input())
-	F=F[Tmp][0]
+def printnames(Text) :
+	Bool = False
+	while Bool == False:
+		cursor.execute(Text)
+		F=cursor.fetchall()
+		if F!=[]:
+			Bool=True
+		else:
+			print('Nafnið er vitlaust eða gögn ekki til')
+		for i in range(len(F)):
+			print(i,F[i][0])
+		Tmp=int(input())
+		F=F[Tmp][0]
 	return F
 print("""Góðan daginn,
 Velkominn í Ultra-Super-Mega-Giga-Undestructable-Uber-Portfolio Calculator
@@ -30,16 +50,16 @@ while Chosen==False:
 	else:
 			print('Veldu eitthvað af valmöguleikunum')
 x=int(x)
-if x==2
+if x==2:
 	Worked=False
 	while Worked==False:
 		x=int(input("""Veldu stærð gagnagrunns fyrir portfolio:\n Heimsálfa(1)\nLand(2)\nMarkað(1)"""))
 		if x==1:
-			F=printnames('select Distinct e.heimsalfa from exchange')
+			F=printnames('select DISTINCT e.heimsalfa from exchange e')
 			Worked=True
 		elif x==2:
-			F=printnames('select Distinct e.land from exchange')
+			F=printnames('select Distinct e.land from exchange e')
 			Worked=True
 		elif x==3:
-			F=printnames('select Distinc e.exch from exchange')
+			F=printnames('select Distinc e.exch from exchange e')
 			Worked=True
