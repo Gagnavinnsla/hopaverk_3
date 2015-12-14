976ba1a1e545beea07cccc0386f7bc5977865a20
omarsindri <omarsindri@hotmail.com>
2015-12-14 14:35:58 +0000

    go

--- yahoo.py
+++ yahoo.py
@@ -1,8 +1,9 @@
-from pandas_datareader import data as web
+import pandas.io.data as web
 import pandas as pd
 import numpy as np
 import datetime
 from datetime import date
+import psycopg2
 
 host = 'localhost'
 dbname = 'stocks'
@@ -24,19 +25,20 @@ today = datetime.datetime.today()
 end = today
 
 
-ticker = pd.read_csv('YahooStockTickerSymbols.csv', sep=';',encoding='utf-8')
-Exchange=pd.read_csv('Exchange.csv',sep=';',encoding='utf-8')
+ticker = pd.read_csv('YahooStockTickerSymbols.csv', sep=';',encoding='utf8')
+#Exchange=pd.read_csv('Exchange.csv',sep=';',encoding='utf-8')
 
-for i in range(len(Exchange)):
-	command="""insert into data (exch,fjoldi,land) values ('{}','{}','{}')""".format(Exchange.iloc[i,0],Exchange.iloc[i,1],Exchange.iloc[i,2],Exchange.iloc[i,3])
-	cursor.execute(command)
+#for i in range(len(Exchange)):
+#	command="""insert into data (exch,fjoldi,land) values ('{}','{}','{}')""".format(Exchange.iloc[i,0],Exchange.iloc[i,1],Exchange.iloc[i,2],Exchange.iloc[i,3])
+#	cursor.execute(command)
 for i in range(len(ticker)):
 	command="""insert into data (ticker,name,exchange,category) values ('{}','{}','{}','{}')""".format(ticker.iloc[i,0],ticker.iloc[i,1],ticker.iloc[i,2],ticker.iloc[i,3])
-	cursor.execute(command)
-	x = web.DataReader(ticker.iloc[i,0],'yahoo',start,end)
-	for i in range(len(x)):
-		command="""insert into data (ticker,date,open,high,low,close,volume, adj close) values ('{}','{}','{}','{}','{}','{}','{}','{}')""".format(ticker,x.index[i],x.iloc[i,0],x.iloc[i,1],x.iloc[i,2],x.iloc[i,3],x.iloc[i,4],x.iloc[i,5])
-		cursor.execute(command)
+	print(command)
+#	cursor.execute(command)
+#	x = web.DataReader(ticker.iloc[i,0],'yahoo',start,end)
+#	for i in range(len(x)):
+#		command="""insert into data (ticker,date,open,high,low,close,volume, adjclose) values ('{}','{}','{}','{}','{}','{}','{}','{}')""".format(ticker,x.index[i],x.iloc[i,0],x.iloc[i,1],x.iloc[i,2],x.iloc[i,3],x.iloc[i,4],x.iloc[i,5])
+#		cursor.execute(command)
 
 conn.commit()
 cursor.close()
