3f279e92c9466c84713f14faed51f4f1e6522ff1
darthvader17 <fannara14@ru.is>
2015-12-15 09:57:27 +0000

    Dsfs

--- keldan.py
+++ keldan.py
@@ -5,7 +5,7 @@ import pandas as pd
 ticker = pd.read_csv('YahooStockTickerSymbols.csv', sep=';',encoding='utf-8')
 
 for i in ticker.iloc[0:16,0]:
-print(i, Share(i).get_price())
+	print(i, Share(i).get_price(), Share(i).get_change())
 
 
 #gætuð þurft að gera pip install yahoo_finance
